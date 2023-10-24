149ca5b496df070e012915e85b01b99fdf423cad
raphex04 <132359802+raphex04@users.noreply.github.com>
2023-10-24 08:55:54 -0300

    Delete ProjetoSwagger directory

--- ProjetoSwagger/src/main/java/com/ProjetoSwagger/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProjetoSwagger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ProjetoSwagger.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository <Cliente, Long>{
-
-}
